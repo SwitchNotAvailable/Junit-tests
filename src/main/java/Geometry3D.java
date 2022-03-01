0f814020f7f32e934089c6a90c574c8ec0033cf4
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-01 20:31:52 +0000

    Corrected method for "Volume of a Square Pyramid"

--- src/main/java/Geometry3D.java
+++ src/main/java/Geometry3D.java
@@ -10,7 +10,7 @@ public class Geometry3D {
     }
 
     public static float volumeSquarePyramid(float baseArea, float height) {
-        float volume = (float) ((1/3) * (Math.pow(baseArea, 2)) * (height));
+        float volume = (float) ((Math.pow(baseArea, 2)) * (height/3));
         return volume;
     }
 
