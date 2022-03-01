b55b17d7b72e86e60db19a14bc53a5e8114fbd40
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-01 20:24:26 +0000

    Created Test for Calculating "Surface area of a cuboid".

--- src/test/java/Geometry3DTest.java
+++ src/test/java/Geometry3DTest.java
@@ -6,11 +6,12 @@ class Geometry3DTest {
 
     @Test
     void volumeCuboid() {
-        assertEquals(453.6, Geometry3D.volumeCuboid(9, (float) 8.4, 6), 0.1);
+        assertEquals(453.6, Geometry3D.volumeCuboid(9, 8.4f, 6), 0.1);
     }
 
     @Test
     void surfaceCuboid() {
+        assertEquals(186.6, Geometry3D.surfaceCuboid(5, 4.1f, 8), 0.1);
     }
 
     @Test
