aa178f8df6ec3e4a0c671b3e496c40c09bddb066
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-01 19:42:27 +0000

    Changed variables from "int" to "long" and created method for Surface Area of a cuboid.

--- src/main/java/Geometry3D.java
+++ src/main/java/Geometry3D.java
@@ -1,7 +1,12 @@
 public class Geometry3D {
-    public static int volumeCuboid(int length, int width, int height) {
-        int volume = length * width * height; //formula of volume of a Cuboid
+    public static float volumeCuboid(float length, float width, float height) {
+        float volume = length * width * height; //formula of volume of a Cuboid
         return volume; //return volume of a cuboid
+    }
+
+    public static float surfaceCuboid(float length, float width, float height) {
+        float surfaceCuboid = (2*length*width) + (2*length*height) + (2*height*width);
+        return surfaceCuboid;
     }
 
 }
