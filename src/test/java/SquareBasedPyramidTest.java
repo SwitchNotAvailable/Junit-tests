fc8bc725e4ed73113a1ec08dc35a10f14fe96fab
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 22:24:48 +0000

    Modified Test methods of Volume and Surface Area to use appropriate class files

--- src/test/java/SquareBasedPyramidTest.java
+++ src/test/java/SquareBasedPyramidTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquareBasedPyramidTest {
 
     @Test
-    void volume() {
-        assertEquals(155.52, Geometry3D.volumeSquarePyramid(7.2f, 9), 0.1);
+    void volume() { //test for volume of a Square Based Pyramid
+        assertEquals(155.52, SquareBasedPyramid.volume(7.2f, 9), 0.1);
     }
 
     @Test
-    void surfaceArea() {
-        assertEquals(744.84, Geometry3D.surfaceSquarePyramid(13.4f, 20), 0.1);
+    void surfaceArea() { //Test for Surface Area of a Square Based Pyramid
+        assertEquals(744.84, SquareBasedPyramid.surfaceArea(13.4f, 20), 0.1);
     }
 }
