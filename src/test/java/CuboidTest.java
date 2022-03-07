eaf7bfb9e6c3bb25ba819d4d5edc2b22befde1ce
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 22:20:05 +0000

    Modified Test methods for volume and Surface Area.

--- src/test/java/CuboidTest.java
+++ src/test/java/CuboidTest.java
@@ -6,11 +6,11 @@ class CuboidTest {
 
     @Test
     void volume() { //Test for volume of a cuboid
-        assertEquals(453.6, Geometry3D.volumeCuboid(9, 8.4f, 6), 0.1);
+        assertEquals(453.6, Cuboid.volume(9, 8.4f, 6), 0.1);
     }
 
     @Test
     void surfaceArea() {
-        assertEquals(186.6, Geometry3D.surfaceCuboid(5, 4.1f, 8), 0.1);
+        assertEquals(186.6, Cuboid.surfaceArea(5, 4.1f, 8), 0.1);
     }
 }
