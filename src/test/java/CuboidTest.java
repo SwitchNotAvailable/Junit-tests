000bac220f8a4e7670b76dd6f88819a730c9b498
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 21:52:50 +0000

    Successfully ran Test for Surface Area of a cuboid in CuboidTest.java Test file

--- src/test/java/CuboidTest.java
+++ src/test/java/CuboidTest.java
@@ -5,11 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class CuboidTest {
 
     @Test
-    void volume() {
+    void volume() { //Test for volume of a cuboid
         assertEquals(453.6, Geometry3D.volumeCuboid(9, 8.4f, 6), 0.1);
     }
 
     @Test
     void surfaceArea() {
+        assertEquals(186.6, Geometry3D.surfaceCuboid(5, 4.1f, 8), 0.1);
     }
 }
