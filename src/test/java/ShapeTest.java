819068d7e93095458f0cc5f7937bc0969639a367
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 21:53:50 +0000

    Created Test for Shape.java file.

---
+++ src/test/java/ShapeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapeTest {
+
+    @Test
+    void volume() {
+    }
+
+    @Test
+    void surfaceArea() {
+    }
+
+    @Test
+    void description() {
+    }
+}
