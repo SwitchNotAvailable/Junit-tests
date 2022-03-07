54297756635cb3498004a1cf1a06c81b9d8befa0
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 22:10:51 +0000

    Created Tetrahedron.ja  file and added a method for calculating the Volume of a Tetrahedron.

---
+++ src/main/java/Tetrahedron.java
@@ -0,0 +1,9 @@
+public abstract class Tetrahedron extends Shape {
+    public Tetrahedron(float length, float width, float height) {
+        super(length, width, height);
+    }
+
+    public float volume(float length) {
+        return (float) ((Math.pow(length, 3)) / (6 * Math.sqrt(2)));
+    }
+}
