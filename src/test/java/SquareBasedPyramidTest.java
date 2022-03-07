99a24b8c1c4d82f6f017e67a2985ab3616a9f323
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 22:07:41 +0000

    Added Test for Surface Area of a Square Based Pyramid to SquareBasedPyramidTest file.

--- src/test/java/SquareBasedPyramidTest.java
+++ src/test/java/SquareBasedPyramidTest.java
@@ -8,4 +8,9 @@ class SquareBasedPyramidTest {
     void volume() {
         assertEquals(155.52, Geometry3D.volumeSquarePyramid(7.2f, 9), 0.1);
     }
+
+    @Test
+    void surfaceArea() {
+        assertEquals(744.84, Geometry3D.surfaceSquarePyramid(13.4f, 20), 0.1);
+    }
 }
