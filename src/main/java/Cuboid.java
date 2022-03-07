eacbe0126fc18972c5cb5574b2d65d0739e79a7f
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 21:16:03 +0000

    Created Cuboid class

---
+++ src/main/java/Cuboid.java
@@ -0,0 +1,15 @@
+public abstract class Cuboid extends Shape {
+
+    public Cuboid(float length, float width, float height) {
+        super(length, width, height);
+    }
+
+    public float volume(float length, float width, float height) { //Method for calculating Volume of a cuboid
+        return length * width * height; //return volume of a cuboid
+    }
+
+    public float surfaceArea(float length, float width, float height) {
+        return (2*length*width) + (2*length*height) + (2*height*width); // return surface area of a cuboid
+    }
+
+}
