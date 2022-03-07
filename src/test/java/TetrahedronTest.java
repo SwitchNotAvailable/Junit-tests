acef652adb12a8467be6c09b86067bb9e90331db
Jeffrey Ikwunze <jeffreykelechi.ikwunze@student.griffith.ie>
2022-03-07 22:30:22 +0000

    Created Test for Surface area of a tetrahedron.

--- src/test/java/TetrahedronTest.java
+++ src/test/java/TetrahedronTest.java
@@ -8,4 +8,9 @@ class TetrahedronTest {
     void volume() { //Test for Volume of a Tetrahedron
         assertEquals(558.81, Tetrahedron.volume(16.8f), 0.1);
     }
+
+    @Test
+    void surfaceArea() { //test for surface area of a tetrahedron
+        assertEquals(1611.24, Tetrahedron.surfaceArea(30.5f), 0.1);
+    }
 }
